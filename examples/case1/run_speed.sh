e6536f8796d1ab9b64840ee88317e0d825823b4c
alessio.fumagalli <alessio.fumagalli1984@gmail.com>
2026-03-05 10:00:28 +0000

    update config

--- examples/case1/run_speed.sh
+++ examples/case1/run_speed.sh
@@ -2,16 +2,23 @@
 
 export SPEED_PROGRAM=SPEED
 export OUTPUT=speed.out 
-export TOTAL_MPI_PROCESSES=1
+export TOTAL_MPI_PROCESSES=4
 export THREADS=1
  
 let NCORES=$TOTAL_MPI_PROCESSES
 echo "Running on $NCORES cores"
 echo "Job started at `date`"
 
+# Create output directories if they don't exist
 mkdir -p VTKOUT FILES_MPI MONITOR POST-PROC 
+
+# Clean up previous output files
 rm -rf VTKOUT/*
-#time mpirun -np $TOTAL_MPI_PROCESSES  -x OMP_NUM_THREADS=$THREADS  $SPEED_PROGRAM >& $OUTPUT
-time $SPEED_PROGRAM >& $OUTPUT
+rm -rf FILES_MPI/*
+rm -rf MONITOR/*
+rm -rf POST-PROC/*
+
+time mpirun -np $TOTAL_MPI_PROCESSES  -x OMP_NUM_THREADS=$THREADS  $SPEED_PROGRAM >& $OUTPUT
+#time $SPEED_PROGRAM >& $OUTPUT
 wait
 
